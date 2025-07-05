a503ee29ea3c9209fafadc69f972aacd25795d95
LYH513 <peperony367@gmail.com>
2025-07-06 01:59:08 +0900

    A007, A008, A009, A010 완료

---
+++ src/A008.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class A008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int C = sc.nextInt();
+
+        for (int t = 0; t < C; t++) {
+            int N = sc.nextInt();
+            int[] scores = new int[N];
+            int sum = 0;
+
+            for (int i = 0; i < N; i++) {
+                scores[i] = sc.nextInt();
+                sum += scores[i];
+            }
+
+            double avg = (double) sum / N;
+            int countAboveAvg = 0;
+
+            for (int score : scores) {
+                if (score > avg) {
+                    countAboveAvg++;
+                }
+            }
+
+            double rate = (double) countAboveAvg / N * 100;
+
+            System.out.printf("%.3f%%\n", rate);
+        }
+    }
+}
---
+++ src/A010.java
@@ -0,0 +1,41 @@
+public class A010 {
+    public int solution(String s) {
+        int answer = Integer.MAX_VALUE;
+
+        if(s.length()==1) return 1;
+
+        for (int i=1; i<= s.length()/2; i++){
+            String pattern = s.substring(0, i);
+            int count = 1;
+            String reStr = "";
+
+            for(int j=i; j <=s.length()-i; j+=i){ // i개의 pattern이니까 i개씩 건너뛰면서 일치여부 확인
+                if(pattern.equals(s.substring(j, j+i))){
+                    count ++;
+                } else { // 글자합치기
+                    if (count >1){
+                        reStr += count+"";
+                    }
+                    reStr += pattern;
+
+                    //초기화
+                    pattern = s.substring(j, j+i);
+                    count =1;
+                }
+            }
+
+            //나머지 계산
+            if (count>1){
+                reStr += ""+count;
+            }
+            reStr += pattern;
+
+            //길이가 가장 작은 값 찾기
+            int div = s.length()% i;
+            answer = Math.min(answer, reStr.length()+div);
+
+        }
+
+        return answer;
+    }
+}
